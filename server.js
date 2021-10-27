235e950ad68b35ce1147b41278ca29a5a22a3fd5
schultzo <olivia-schultz@hotmail.com>
2021-10-28 11:33:42 +1300

    Initial Set up

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+
+const server = express()
+
+module.exports = server
+
+
+// Middleware
+server.engine('hbs', hbs({
+    extname: 'hbs'
+  }))
+  server.set('view engine', 'hbs')
+  server.use(express.static('public'))
+
+
+
+  server.get("/",(req, res)=>{
+      res.render('home.hbs')
+  })
